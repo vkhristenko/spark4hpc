c44e3b0faff6c55d7121cd1ab3fcdbe8d813d42c
vkhristenko <vdkhristenko1991@gmail.com>
2018-02-21 12:35:09 +0100

    simple test example

---
+++ tests/startCluster.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#
+# set the env vars 
+#
+SPARK_HOME=$1
+SPARK_MASTER_IP=$2
+SPARK_MASTER_PORT=7077
+JOBID=$SLURM_JOB_ID
+TASKID=$SLURM_PROCID
+
+if [ $TASKID -eq 0 ]; then 
+    echo "Starting a SPARK MASTER on node ${SPARK_MASTER_IP} and the hostname is $(hostname)"
+else
+    echo "Starting a SPARK WORKER on node $(hostname)"
+fi
