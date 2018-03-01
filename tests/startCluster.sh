a565cacd0b21690de6c4dd12a4ef369f1cfe45ae
vkhristenko <vdkhristenko1991@gmail.com>
2018-03-01 14:56:21 +0100

    update

--- tests/startCluster.sh
+++ tests/startCluster.sh
@@ -12,7 +12,12 @@ TASKID=$SLURM_PROCID
 if [ $TASKID -eq 0 ]; then 
     echo "Starting a SPARK MASTER on node ${SPARK_MASTER_IP} and the hostname is $(hostname)"
     $SPARK_HOME/sbin/start-master.sh
+    eval jps
+    sleep 1m
 else
     echo "Starting a SPARK WORKER on node $(hostname)"
-    sleep 30s
+    sleep 10s
+    $SPARK_HOME/sbin/start-slave.sh spark://$SPARK_MASTER_IP:$SPARK_MASTER_PORT
+    eval jps
+    sleep 10s
 fi
